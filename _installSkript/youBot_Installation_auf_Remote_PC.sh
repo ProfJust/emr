755d64e56d8d4fa90b8d34d8e5396dd583c966c0
Prof. Just <olaf.just@w-hs.de>
2020-03-24 21:55:10 +0100

    new

--- _installSkript/youBot_Installation_auf_Remote_PC.sh
+++ _installSkript/youBot_Installation_auf_Remote_PC.sh
@@ -22,8 +22,12 @@ git clone http://github.com/wnowak/brics_actuator.git
 git clone https://github.com/pschillinger/youbot_integration.git
 git clone https://github.com/pal-robotics-forks/point_cloud_converter
 git clone https://github.com/team-vigir/flexbe_behavior_engine.git
-git clone https://github.com/wnowak/youbot_applications.git
 git clone https://github.com/wnowak/youbot_moveit.git
+git clone https://github.com/wnowak/youbot_applications.git
+# android_app needs BLUETOOTH_INCLUDE_DIR => delete directory
+cd catkin_ws/src/youbot_applications/
+rm -r  android_app_pc_client
+rm -r keyboard_remote_control
 
 sudo apt-get dist-upgrade -y   #-y ist ohne Ja Abfrage
 sudo apt-get update -y
@@ -34,23 +38,40 @@ sudo apt-get install ros-melodic-slam-gmapping -y
 sudo apt-get install ros-melodic-amcl -y
 sudo apt-get install ros-melodic-move-base -y
 
-echo -e "\033[31m Erstelle catkin_pkg \033[0m"
+echo -e "\033[34m Erstelle catkin_pkg \033[0m"
 catkin_create_pkg emr std_msgs rospy roscpp
 
-echo -e "\033[31m Aktualisiere alle Abhaengigkeiten der ROS-Pakete \033[0m"
+echo -e "\033[34m Aktualisiere alle Abhaengigkeiten der ROS-Pakete \033[0m"
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
 
-sudo setcap cap_net_raw+ep devel/lib/youbot_driver_ros_interface/youbot_driver_ros_interface
+sudo setcap cap_net_raw+ep ~/catkin_ws/devel/lib/youbot_driver_ros_interface/youbot_driver_ros_interface
 
-echo -e "\033[31m to do: add at bottom ~/catkin_ws/src/youbot_navigation/youbot_navigation_common/CMakeLists.txt
-Z14
+echo -e "\033[34m add at bottom ~/catkin_ws/src/youbot_navigation/youbot_navigation_common/CMakeLists.txt \033[0m" 
+#Z14
 ## by OJ since ros.h was not found
-INCLUDE_DIRECTORIES(
-	include
-	${catkin_INCLUDE_DIRS}
-)
-\033[0m" 
+#INCLUDE_DIRECTORIES(
+#	include
+#	${catkin_INCLUDE_DIRS}
+#)
 
-echo -e "\033[31m to do:   $ cd ~/catkin_ws/  ...   catkin_make \033[0m"
+
+## Erganze Zeilen in CMakeLists since catkion_make says ros.h was not found
+cd ~/catkin_ws/src/youbot_navigation/youbot_navigation_common
+echo "## by OJ since ros.h was not found" >> CMakeLists.txt
+echo "INCLUDE_DIRECTORIES(" >> CMakeLists.txt
+echo "include" >> CMakeLists.txt
+echo "${catkin_INCLUDE_DIRS}" >> CMakeLists.txt
+echo ")" >> CMakeLists.txt
+
+
+
+echo -e "\033[34m to do:   $ cd ~/catkin_ws/  ...   catkin_make \033[0m"
+
+cd ~/catkin_ws/
+catkin_make
+
+echo -e "\033[34m EMR - SS20 - Workspace is installed - have fun!  \033[0m"
+
+echo -e "\033[32m $ roslaunch emr youbot_emr_simulation.launch \033[0m"
 
