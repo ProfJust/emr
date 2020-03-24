273b10b92dd31a696a94a72361004661299860f9
Prof. Just <olaf.just@w-hs.de>
2020-03-24 22:59:07 +0100

    Add files via upload

--- _installSkript/youBot_Installation_auf_Remote_PC.sh
+++ _installSkript/youBot_Installation_auf_Remote_PC.sh
@@ -17,17 +17,20 @@ git clone http://github.com/youbot/youbot_navigation -b hydro-devel
 git clone http://github.com/youbot/youbot_driver -b hydro-devel
 git clone http://github.com/youbot/youbot_driver_ros_interface.git -b indigo-devel
 git clone http://github.com/youbot/youbot_description.git -b jade-devel
+#Alternative? git clone https://github.com/mas-group/youbot_description.git
 git clone http://github.com/youbot/youbot_simulation.git
+#Alternative? git clone https://github.com/mas-group/youbot_simulation.git
 git clone http://github.com/wnowak/brics_actuator.git
 git clone https://github.com/pschillinger/youbot_integration.git
+git clone https://github.com/FlexBE/youbot_behaviors.git
 git clone https://github.com/pal-robotics-forks/point_cloud_converter
 git clone https://github.com/team-vigir/flexbe_behavior_engine.git
 git clone https://github.com/wnowak/youbot_moveit.git
-git clone https://github.com/wnowak/youbot_applications.git
+# braucht man nicht wirklich: git clone https://github.com/wnowak/youbot_applications.git
 # android_app needs BLUETOOTH_INCLUDE_DIR => delete directory
-cd catkin_ws/src/youbot_applications/
-rm -r  android_app_pc_client
-rm -r keyboard_remote_control
+#cd catkin_ws/src/youbot_applications/
+#rm -r  android_app_pc_client
+#rm -r keyboard_remote_control
 
 sudo apt-get dist-upgrade -y   #-y ist ohne Ja Abfrage
 sudo apt-get update -y
@@ -38,6 +41,7 @@ sudo apt-get install ros-melodic-slam-gmapping -y
 sudo apt-get install ros-melodic-amcl -y
 sudo apt-get install ros-melodic-move-base -y
 
+cd ~/catkin_ws/src
 echo -e "\033[34m Erstelle catkin_pkg \033[0m"
 catkin_create_pkg emr std_msgs rospy roscpp
 
@@ -57,7 +61,7 @@ echo -e "\033[34m add at bottom ~/catkin_ws/src/youbot_navigation/youbot_navigat
 
 
 ## Erganze Zeilen in CMakeLists since catkion_make says ros.h was not found
-cd ~/catkin_ws/src/youbot_navigation/youbot_navigation_common
+cd ~/catkin_ws/src/youbot_navigation/youbot_navigation_common/
 echo "## by OJ since ros.h was not found" >> CMakeLists.txt
 echo "INCLUDE_DIRECTORIES(" >> CMakeLists.txt
 echo "include" >> CMakeLists.txt
