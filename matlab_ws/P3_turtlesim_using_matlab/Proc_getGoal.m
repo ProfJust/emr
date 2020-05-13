19a23880518dc4e674b3d5a99517c8eb34eeb35e
Prof. Just <olaf.just@w-hs.de>
2020-05-13 13:12:14 +0200

    Delete Proc_getGoal.m

--- matlab_ws/P3_turtlesim_using_matlab/Proc_getGoal.m
+++
@@ -1,5 +0,0 @@
- % Proc_getGoal.m
- %-------------------------------
-goalX = input('Ziel X,'s'); % Benutzereingabe
-goalY = input('Ziel Y,'s'); % Benutzereingabe
-goalT = input('Ziel Theta,'s'); % Benutzereingabe
