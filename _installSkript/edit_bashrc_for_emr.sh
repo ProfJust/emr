ab6a8c94046b7bba21f535a57a7263f0e18ee517
Prof. Just <olaf.just@w-hs.de>
2020-05-19 13:12:08 +0200

    Add files via upload

--- _installSkript/edit_bashrc_for_emr.sh
+++ _installSkript/edit_bashrc_for_emr.sh
@@ -15,16 +15,29 @@ cd ~/
 
 
 ## Erganze Zeilen in CMakeLists since catkion_make says ros.h was not found
-echo "###  ROS - Umgebungsvariablen setzen ###" >> .bashrc
-echo "source /opt/ros/melodic/setup.bash" >> .bashrc
-echo "## IP des PCs auf dem der Master laeuft ## " >> .bashrc
-echo "## Gazebo => localhost " >> .bashrc
-echo "export ROS_MASTER_URI=http://localhost:11311/" >> .bashrc
-echo "## IP dieses Rechners " >> .bashrc
-echo "export ROS_IP=192.168.129.104" >> .bashrc
-echo "ROS_PACKAGE_PATH=~/catkin_ws/src:/opt/ros/meloduc/share"
-echo "source devel/setup.bash" >> .bashrc
+#echo "###  ROS - Umgebungsvariablen setzen ###" >> .bashrc
+#echo "source /opt/ros/melodic/setup.bash" >> .bashrc
+#echo "## IP des PCs auf dem der Master laeuft ## " >> .bashrc
+#echo "## Gazebo => localhost " >> .bashrc
+#echo "export ROS_MASTER_URI=http://localhost:11311/" >> .bashrc
+#echo "## IP dieses Rechners " >> .bashrc
+#echo "export ROS_IP=192.168.129.104" >> .bashrc
+#echo "ROS_PACKAGE_PATH=~/catkin_ws/src:/opt/ros/meloduc/share"
+#echo "source ~/catkin_ws/devel/setup.bash" >> .bashrc
 
+echo "###  ROS - Umgebungsvariablen setzen ###
+source /opt/ros/melodic/setup.bash
+source ~/catkin_ws/devel/setup.bash
+
+export ROS_PACKAGE_PATH=~/catkin_ws/src:/opt/ros/melodic/share
+# local =>Gazebo
+export ROS_MASTER_URI=http://127.0.0.1:11311
+export ROS_HOSTNAME=127.0.0.1
+# global => real youBot
+# export ROS_MASTER_URI=http://192.168.0.40:11311
+# export ROS_HOSTNAME=192.168.0.41
+###############################################
+" >> .bashrc
 #echo "export PYTHONPATH=$PYTHONPATH:$ROS_ROOT/core/roslib/src" >> .bashrc
 #Avoid variable substitution with /var or 'var '
 #echo "\${catkin_INCLUDE_DIRS}" >> CMakeLists.txt
