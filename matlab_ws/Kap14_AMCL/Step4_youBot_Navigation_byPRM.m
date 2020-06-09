5325318a76f5fc2ec41ddfc3a4f0bf7d7440e67a
Prof. Just <olaf.just@w-hs.de>
2020-06-09 12:09:15 +0200

    Add files via upload

---
+++ matlab_ws/Kap14_AMCL/Step4_youBot_Navigation_byPRM.m
@@ -0,0 +1,97 @@
+% Step4_youBot_Navigation_byPRM.m
+%---------------------------------------------------------------
+% starten des zugehoerigen Launch-Skriptes fuer Gazebo:
+% roslaunch emr_worlds youbot_arena.launch 
+% .d.h. youBot in der Arena des Robotik-Labors
+%--------------------------------------------------------------
+% 9.06.2020
+%---------------------------------------------------------------
+%% -- ROS Init --
+ROS_Node_init_localhost;
+subOdom = rossubscriber ('odom', 'nav_msgs/Odometry');
+pubVel  = rospublisher  ('cmd_vel', 'geometry_msgs/Twist');
+msgsBaseVel = rosmessage(pubVel);
+
+%% ---- Karte laden ----
+mapInflated = load('myArenaMap.mat');
+% Aufblasen (inflate) der Map
+youBotRadiusGrid = 8; %Aufblasen auf youBot-Breite default 15
+disp('Inflate Map ...');
+inflate(mapInflated.map,youBotRadiusGrid,'grid');
+show(mapInflated.map);
+hold on;
+% Find a path between the start and end location.
+ posedata_quat = receive(subOdom,10);
+% pose in Euler umrechnen (mit Versatz)
+% estimated Pose aus Step3 liegt im Workspace
+estimatedPose = youBot_Pose_Quat_2_Eul(posedata_quat);
+if not(exist('estimatedPose'))
+    estimatedPose = [0 0 0];
+end
+startLocation = [estimatedPose(1) estimatedPose(2)] % X,Y
+initialOrientation = estimatedPose(3); % Z
+
+%% -- endLocation is choosen by user
+  disp('Ziel mit Maus auf Karte waehlen');
+  endLocation = ginput(1)
+
+%% --- PRM ---
+prm = robotics.PRM(mapInflated.map);
+prm.NumNodes = 500;
+prm.ConnectionDistance = 10;
+disp('Suche PRM-Pfad ...');
+path = findpath(prm, startLocation, endLocation)
+
+if isempty(path)      
+    disp('kein Pfad gefunden...');
+    %Falls kein Pfad gefunden wurde nochmal
+       prm.NumNodes = prm.NumNodes+100;
+       prm.ConnectionDistance = prm.ConnectionDistance+5;
+       prm = robotics.PRM(mapInflated.map);
+end
+show( prm, 'Map', 'on', 'Roadmap', 'on');
+%Pfad folgen mit PurePursuit-Controller
+controller = robotics.PurePursuit;
+controller.DesiredLinearVelocity = 0.5;
+controller.MaxAngularVelocity = 0.4;
+controller.LookaheadDistance = 0.5;
+controller.Waypoints = path;
+
+robotCurrentLocation = path(1,:);
+robotGoal = path(end,:);
+robotCurrentPose = [robotCurrentLocation initialOrientation];
+distanceToGoal = norm(robotCurrentLocation - robotGoal);
+goalRadius = 0.5;
+disp('Fahre PRM-Pfad ...');
+
+%% -------------------------------------------------------------------
+while( distanceToGoal > goalRadius )
+    % pose holen und speichern
+     posedata_quat = receive(subOdom,10);
+     % pose in Euler umrechnen (mit Versatz)
+     robotCurrentPose = youBot_Pose_Quat_2_Eul(posedata_quat);
+    % Compute the controller outputs, i.e., the inputs to the robot
+    %[v, omega] = controller(robot.getRobotPose);
+    %#################  verbesserter PurePursuit ####################
+    [v_x, v_y, omega] = step_PurePursuit_youbot(controller, robotCurrentPose);
+    
+    % Simulate the robot using the controller outputs
+    % drive(robot, v, omega);
+    msgsBaseVel.Linear.X  = v_x;
+    msgsBaseVel.Linear.Y  = v_y;
+    msgsBaseVel.Angular.Z = omega;
+    send(pubVel ,msgsBaseVel)    
+    % Re-compute the distance to the goal
+    distanceToGoal = norm(robotCurrentPose(1:2) - robotGoal)
+end
+
+%% --- Finally --
+disp('##### Ziel erreicht ####');
+disp(robotCurrentPose);
+estimatedPose = robotCurrentPose;
+beep
+%Robot Anhalten
+msgsBaseVel.Linear.X=0;
+msgsBaseVel.Linear.Y=0;
+msgsBaseVel.Angular.Z=0;
+send(pubVel,msgsBaseVel);
---
+++ matlab_ws/Kap14_AMCL/step_PurePursuit_youbot.m
@@ -0,0 +1,150 @@
+function [v_x,v_y,omega] = step_PurePursuit_youbot(pp, cpose, orie, kv_rot, kv_ang)
+%%%%------------PurePursuit algorithmus angepasst auf den Kuka-Youbot--------------%%%%
+%%%%-------------------Westfälische hochschule Juni/Juli 2017----------------------%%%%
+%%%%---------------------Projektarbeit: Embedded Robotics--------------------------%%%%
+%%%%--------------Betreuender Professor: Prof. Dr. Olaf Just-----------------------%%%%
+%------Authoren------%:
+% Marcel Bohnenkamp
+% Marcel Herzig  
+% Tobias Wewers
+
+%%%%------------Begriffe--------------%%%%
+%WP1: Wegpunkt der näher am Startpunkt als am Zielpunkt liegt
+
+%%%%------------Optionale Parameter, Default Werte--------------%%%%
+if nargin < 3
+    %Verstärkungsfaktor gibt an wie Stark die Rotation geregelt wird
+    orie = 0;
+end
+if nargin < 4
+    %Verstärkungsfaktor gibt an wie Stark die Rotation priorisiert wird
+    kv_rot = 5;
+end
+if nargin < 5
+    %Verstärkungsfaktor gibt an wie Stark die Rotation geregelt wird
+    kv_ang = 15;
+end
+
+%naheliegendste Position auf Pfad finden
+wp = pp.Waypoints;
+s = [inf,0,0,0]; %[Strecke von Cpose-Schnittpunkt, WP1, [X,Y]]
+for i=1:(length(wp)-1)
+    s_temp = calc_vektorschnittpunkt(wp(i,:),wp(i+1,:),cpose(1:2));
+    %Senkrechte zu Pfad
+    if ~isnan(s_temp)
+        if norm(cpose(1:2)-s_temp) < s(1)
+            s = [norm(cpose(1:2)-s_temp),i,s_temp];
+        end
+    end
+    %Abstand zu Punkt
+    if norm(cpose(1:2)-wp(i,:)) < s(1)
+        s = [norm(cpose(1:2)-wp(i,:)),i,wp(i,:)];
+    end
+end
+
+%Look-Ahead Punkt bestimmen
+wp_spnr=1;
+%Pfad Rückwärts durchgehen bis Schnittpunkt(e) gefunden
+for i = (length(wp)-1):-1:s(2)
+    v_pfad = wp(i+1,:)-wp(i,:);
+    if i == length(wp)-1
+        %Letzen Pfad-V verlängern damit Look-Ahead auch am Schluss möglich ist
+        v_pfad = (pp.LookaheadDistance+norm(v_pfad))*v_pfad/norm(v_pfad);
+    end
+    [ks1,ks2] = calc_kreisschnittpunkte(wp(i,:),v_pfad,[s(3),s(4)],pp.LookaheadDistance);
+    if (norm(ks1) ~= inf) || (norm(ks2) ~= inf)
+        %WP1-Nummer auf dem ein Kreisschnittpunkt liegt
+        wp_spnr = i;
+        break;
+    end
+end
+
+%Punktauswahl
+p = wp(wp_spnr+1,:);
+if norm(ks1-p) < norm(ks2-p)
+    lah_p = ks1;
+else
+    lah_p = ks2;
+end
+
+%Winkel zwischen cpose und LAh-Vektor + Soll-Orientierung berechnen (alpha)
+theta = cpose(3)+orie*pi/180;
+v_cpose_lah = [lah_p-cpose(1,1:2),0];
+v_temp = [1,0];
+R = [cos(-theta) sin(theta); -sin(theta) cos(-theta)];
+v_ori = [v_temp*R,0];
+alpha = atan2(norm(cross(v_ori,v_cpose_lah)),dot(v_ori,v_cpose_lah))*180/pi;
+
+%Rotatorische Gesch. bestimmen
+v_cross = cross(v_ori,v_cpose_lah); %Drehrichtung
+if v_cross(3) > 0
+    vz = 1;
+else
+    vz = -1;
+end
+%Reglung Roation
+omega = vz*pp.MaxAngularVelocity*kv_ang*abs(alpha/180);
+%Begrenzung 
+if abs(omega) > pp.MaxAngularVelocity
+    omega = (omega/abs(omega))*pp.MaxAngularVelocity;
+end
+
+%Reglung Priorität der Rotation
+omega2 = vz*pp.MaxAngularVelocity*kv_rot*abs(alpha/180);
+%Begrenzung
+if abs(omega2) > pp.MaxAngularVelocity
+    omega2 = (omega2/abs(omega2))*pp.MaxAngularVelocity;
+end
+
+%Translatorische Gesch. bestimmen
+theta = cpose(3);
+v_vel = v_cpose_lah*pp.DesiredLinearVelocity/norm(v_cpose_lah);
+R = [cos(theta) sin(-theta); -sin(-theta) cos(theta)];
+v_vel = v_vel(1:2)*R;
+%Rotation Priorisieren
+v_vel = v_vel*abs(abs(omega2/pp.MaxAngularVelocity)-1);
+%Begrenzung
+if norm(v_vel) > pp.DesiredLinearVelocity
+    v_vel = v_vel*pp.DesiredLinearVelocity/norm(v_vel);
+end
+v_x = v_vel(1);
+v_y = v_vel(2);
+end
+
+function [s1,s2] = calc_kreisschnittpunkte(vo,v,vok,r)
+%vo: Orstvektor vom Pfad
+%v: Pfadvektor
+%vok: Naheliegenster Pfadpunkt/Kreismittelpunkt
+%r: Radius
+%Gleichung: ((vo1+v1*f)-vok1)^2+((vo2+v2*f)-vok2)^2=r^2
+t1 = (-2*vok(1)*v(1)-2*vok(2)*v(2)+2*vo(1)*v(1)+2*vo(2)*v(2))^2;
+t2 = -4*(v(1)^2+v(2)^2)*(vok(1)^2-2*vok(1)*vo(1)+vok(2)^2-2*vok(2)*vo(2)-r^2+vo(1)^2+vo(2)^2);
+t3 = vok(1)*v(1)+vok(2)*v(2)-vo(1)*v(1)-vo(2)*v(2);
+%f1/f2 = Streckungsfaktoren vom Pfad Vektor
+f1 = ((1/2)*sqrt(t1+t2)+t3)/(v(1)^2+v(2)^2);
+f2 = ((-1/2)*sqrt(t1+t2)+t3)/(v(1)^2+v(2)^2);
+
+%Schnittpunkt auf Pfad? (Streckung 0<f<1)
+if isreal(f1) && (f1<=1) && (f1>=0)
+    s1 = vo+v*f1;
+else
+    s1 = [inf,inf];
+end
+if isreal(f2) && (f2<=1) && (f2>=0)
+    s2 = vo+v*f2;
+else
+    s2 = [inf,inf];
+end
+end
+
+function [s] = calc_vektorschnittpunkt(p1,p2,pos)
+v_pfad = p2-p1;
+s = NaN;
+%Senkrechte Pfad/CPose berechen und nötige Streckung bis zum Schnittpunkt
+%bestimmen
+f = (-p1(1)*v_pfad(1)-p1(2)*v_pfad(2)+pos(1)*v_pfad(1)+pos(2)*v_pfad(2))/(v_pfad(1)^2+v_pfad(2)^2);
+%Liegt Schnittpunkt Senkrechte/Pfad auf Pfad? (Streckung 0<f<1)
+if (f>=0) && (f<1)
+    s = p1+v_pfad*f;
+end
+end
