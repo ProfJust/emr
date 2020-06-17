4ca7087a60b6ed96ad502076c4344194c894e152
Prof. Just <olaf.just@w-hs.de>
2020-06-17 08:17:13 +0200

    Add files via upload

---
+++ matlab_ws/P9_real_youbot_arena/Step3_real_youBot_Navigation_byAMCL.m
@@ -0,0 +1,156 @@
+%%  AUTONOMOUS NAVIGATION OF YOUBOT by AMCL
+% Step3_youBot_Navigation_byAMCL.m
+%   The following programm navigates the youBot to a X-Y-position
+%   provided by the user. It is using a modified PurePursuit path tracking
+%   algorithm and Probabilistic Roadmaps (PRM). In order to avoid obstacle
+%   contact, the map is inflated before operation. Furthermore, the
+%   position of the robot is constantly estimated through the Monte Carlo
+%   Localisation algorithm. The map is plotted along with the position of
+%   the youBot and its path to the provided goal location.
+%   The procedure can be repeated until termination by user.
+%
+% Authors of this script:  Marcel Bohnenkamp,  Marcel Herzig, Tobias Wewers
+%
+% Documentation:    <Map preparation>           https://goo.gl/Zdsmuh
+%                   <Map preparation>           https://goo.gl/PbDbC7
+%                   <Monte Carlo Localization>  https://goo.gl/uRTDrm
+%                   <Monte Carlo Localization>  https://goo.gl/5EA8Sf
+%                   <Path building>             https://goo.gl/dufP4a
+%                   <Path building>             https://goo.gl/ERKiZ9
+%                   <Controller>                https://goo.gl/8tXWVU
+%                   <Controller>                https://goo.gl/sMLcjE
+%
+%
+% ------------------------------------------
+% Version vom 9.06.2020 edited for youBot by OJ
+% robotik.bocholt@w-hs.de
+% ------------------------------------------
+close all; %figures
+clear; %workspace
+
+%% ROS Init
+try
+    rosnode list
+catch exp   % Error from rosnode list
+    %  realer youBot
+    rosinit('http://192.168.0.30:11311','NodeName','/RoboLabHome2')  
+end
+subOdom = rossubscriber ('odom', 'nav_msgs/Odometry');
+subScan = rossubscriber('base_scan','sensor_msgs/LaserScan');
+pubVel = rospublisher ('cmd_vel', 'geometry_msgs/Twist');
+msgsBaseVel = rosmessage(pubVel);
+
+%% Konfiguration 
+usedMapName ='realArenaMap.mat' % Die Karte der aktuellen Welt
+mapOccGrid  = load(usedMapName);
+mapInflated = load(usedMapName);
+originalMap = load(usedMapName);
+youbotRadiusGrid = 6; % 33; %Define youBotSize
+numUpdate = 90; % Max. Anzahl der AMCL Update Zyklen
+
+% -- Erster LaserScan => Konfiguration 
+    scanMsg = receive(subScan);    
+% -- Setup Motion Model --
+    % motion can be estimated using odometry data
+    odometryModel = robotics.OdometryMotionModel;
+    %The Noise property defines the uncertainty in robot's rotational and linear motion.
+    odometryModel.Noise = [0.2 0.2 0.2 0.2];
+% -- Setup Sensor Model  rangeFinderModel --
+    rangeFinderModel = robotics.LikelihoodFieldSensorModel;
+    rangeFinderModel.Map = originalMap.map;
+    %The property SensorLimits defines the minimum and maximum range of sensor readings.
+    rangeFinderModel.SensorLimits = [0.45 double(scanMsg.RangeMax)];  % 5.6 im gazebo youBot
+    rangeFinderModel.NumBeams = numel(scanMsg.Ranges); %Gazebo 150 , real 726
+    % Versatz des Laserscanners zur Drehachse berücksichtigen
+    rangeFinderModel.SensorPose = [0.338 0 0];
+%-- Initialize AMCL Object --
+    amcl = robotics.MonteCarloLocalization;
+    amcl.MotionModel = odometryModel;
+    amcl.SensorModel = rangeFinderModel;
+    % Mindestverfahrweg fuer Update
+    amcl.UpdateThresholds = [0.1,0.1,0.1];
+    amcl.ResamplingInterval = 1;
+    amcl.ParticleLimits = [500 50000];
+    amcl.GlobalLocalization = false; % true;
+    %true; %set false if Initial youBotPosition is well estimated
+
+
+%% MONTE CARLO LOCALIZATION (MCL) - Algorithm
+%MCL - https://de.mathworks.com/help/robotics/examples/localize-turtlebot-using-monte-carlo-localization.html
+% Check whether mapInflated is already available
+if exist('mapInflated','var')
+    disp('## Map is up to date ##');
+else
+    disp('## Map Inflation ##');
+    inflate(mapInflated,youbotRadiusGrid,'grid');
+    % Randomly generate NODES
+    prm = robotics.PRM(mapInflated);
+    prm.NumNodes = 214; % number of nodes
+    prm.ConnectionDistance = 1; % in meters
+end
+
+%% GET INITIAL POSE FROM ODOMETRY FOR AMCL
+posedata_quat = receive(subOdom,10);
+% pose in Euler umrechnen (mit Versatz)
+InitialPose = youBot_Pose_Quat_2_Eul(posedata_quat);
+amcl.InitialPose = InitialPose;
+amcl.InitialCovariance = eye(3)*0.5;
+visualizationHelper = ExampleHelperAMCLVisualization(mapInflated.map);
+
+%% --------------------------
+cntUpdate=0;
+scanCnt=0;
+while cntUpdate < numUpdate
+    scanCnt=scanCnt+1;
+    % Receive laser scan and odometry message.
+    scanMsg = receive(subScan);
+    % Create lidarScan object to pass to the AMCL object.
+    scan = lidarScan(scanMsg); 
+    % OdomPose als erste Schätzung holen und speichern
+    posedata_quat = receive(subOdom,10);
+     % pose in Euler umrechnen (mit Versatz)
+    pose = youBot_Pose_Quat_2_Eul(posedata_quat);
+     
+    % Update estimated robot's pose and covariance using new odometry and
+    % sensor readings
+    [isUpdated,estimatedPose, estimatedCovariance] = ...
+        amcl(pose, scan.Ranges, scan.Angles);
+    % Drive robot to next pose. (youBot still moving)
+    if isUpdated 
+        cntUpdate = cntUpdate + 1;
+        disp('cntUpdate: '); disp(cntUpdate);
+        plotStep(visualizationHelper, amcl, estimatedPose, scan, cntUpdate)
+        if cntUpdate==1
+            % Create grid
+            grid minor
+            set(gca,'Xcolor',[1 0 0]); % RGB
+            set(gca,'Ycolor',[1 0 0]);
+            set(gca,'LineWidth', 2); % Line Width
+        end
+    end   
+    disp('estimatedCovariance: ');
+    disp(estimatedCovariance(3, 3))
+    if estimatedCovariance(3, 3)<= 0.065 % probably equals good estimation, but wasn't tested thoroughly
+        disp('#### !!!! Pose found ####');
+        break;
+    else
+        disp('Move youBot with rqt or teleop to help estimating position')
+    end    
+end
+
+%% Finally Stop Robot
+msgsBaseVel.Linear.X = 0;
+msgsBaseVel.Linear.Y = 0;
+msgsBaseVel.Angular.Z = 0;
+send(pubVel,msgsBaseVel);
+
+beep() %Produce operating system beep sound
+disp('## Done Estimating - stop youBot ##')
+disp('reale Pose (Odometry)');
+disp(pose);
+disp('Estimated Pose (AMCL)');
+disp(estimatedPose);
+
+
+
+
---
+++ matlab_ws/P9_real_youbot_arena/Step4_real_youBot_odom_based_Navigation_byPRM.m
@@ -0,0 +1,105 @@
+% Step4_real_youBot_Navigation_byPRM.m
+%---------------------------------------------------------------
+% starten des zugehoerigen Launch-Skriptes: 
+% roslaunch ???
+% .d.h. youBot in der Arena des Robotik-Labors
+
+%% Achtung: Probleme mit der Odometrie beim realen youBot
+%  unbedingt die pose Estimation mit AMCL durchführen
+%  Initial Pose ggf. mit ginput()
+% Workaround, den youBot auf Odom 0,0,0 stellen 
+%--------------------------------------------------------------
+% 16.06.2020
+%---------------------------------------------------------------
+%% -- ROS Init --
+try
+    rosnode list
+catch exp   % Error from rosnode list
+    %  realer youBot
+    rosinit('http://192.168.0.30:11311','NodeName','/RoboLabHome2')  
+end
+subOdom = rossubscriber ('odom', 'nav_msgs/Odometry');
+pubVel  = rospublisher  ('cmd_vel', 'geometry_msgs/Twist');
+msgsBaseVel = rosmessage(pubVel);
+
+%% ---- Karte laden ----
+goalRadius = 0.5;
+mapInflated = load('realArenaMap.mat');
+% Aufblasen (inflate) der Map
+youBotRadiusGrid = 7; %Aufblasen auf youBot-Breite default 15
+disp('Inflate Map ...');
+inflate(mapInflated.map,youBotRadiusGrid,'grid');
+show(mapInflated.map);
+hold on;
+% Find a path between the start and end location.
+ posedata_quat = receive(subOdom,10);
+% pose in Euler umrechnen (mit Versatz)
+% estimated Pose aus Step3 liegt im Workspace
+estimatedPose = youBot_Pose_Quat_2_Eul(posedata_quat); %ohne Versatz rechnen !!
+if not(exist('estimatedPose'))
+    estimatedPose = [-2 1 -1.7];
+end
+startLocation = [estimatedPose(1) estimatedPose(2)] % X,Y
+initialOrientation = estimatedPose(3); % Z
+
+%% -- endLocation is choosen by user
+  disp('Ziel mit Maus auf Karte waehlen');
+  endLocation = ginput(1)
+
+%% --- PRM ---
+prm = robotics.PRM(mapInflated.map);
+prm.NumNodes = 500;
+prm.ConnectionDistance = 10;
+disp('Suche PRM-Pfad ...');
+path = findpath(prm, startLocation, endLocation)
+
+if isempty(path)      
+    disp('kein Pfad gefunden...');
+    %Falls kein Pfad gefunden wurde nochmal
+       prm.NumNodes = prm.NumNodes+100;
+       prm.ConnectionDistance = prm.ConnectionDistance+5;
+       prm = robotics.PRM(mapInflated.map);
+end
+show( prm, 'Map', 'on', 'Roadmap', 'on');
+%Pfad folgen mit PurePursuit-Controller
+controller = robotics.PurePursuit;
+controller.DesiredLinearVelocity = 0.5;
+controller.MaxAngularVelocity = 0.4;
+controller.LookaheadDistance = 0.5;
+controller.Waypoints = path;
+
+robotCurrentLocation = path(1,:);
+robotGoal = path(end,:);
+robotCurrentPose = [robotCurrentLocation initialOrientation];
+distanceToGoal = norm(robotCurrentLocation - robotGoal);
+
+disp('Fahre PRM-Pfad ...');
+
+%% -------------------------------------------------------------------
+while( distanceToGoal > goalRadius )
+    % pose holen und speichern
+     posedata_quat = receive(subOdom,10);
+     % pose in Euler umrechnen (mit Versatz)
+     robotCurrentPose = youBot_Pose_Quat_2_Eul(posedata_quat);
+    % Compute the controller outputs, i.e., the inputs to the robot
+     [v_x, v_y, omega] = step_PurePursuit_youbot(controller, robotCurrentPose);
+    
+    % Drive the robot using the controller outputs
+    msgsBaseVel.Linear.X  = v_x;
+    msgsBaseVel.Linear.Y  = v_y;
+    msgsBaseVel.Angular.Z = omega;
+    send(pubVel ,msgsBaseVel)    
+    % Re-compute the distance to the goal
+    distanceToGoal = norm(robotCurrentPose(1:2) - robotGoal)
+end
+
+%% --- Finally --
+disp('##### Ziel erreicht ####');
+disp(robotCurrentPose);
+estimatedPose = robotCurrentPose;
+beep
+%Robot Anhalten
+msgsBaseVel.Linear.X=0;
+msgsBaseVel.Linear.Y=0;
+msgsBaseVel.Angular.Z=0;
+send(pubVel,msgsBaseVel);
