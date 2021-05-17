98d8214d2185fca77cd3ea70569847e48d833124
ProfJust <olaf.just@w-hs.de>
2021-05-17 16:08:31 +0200

    joy teleop added

---
+++ youBot_teleop_joy/youBot_teleop_joy.cpp
@@ -0,0 +1,65 @@
+//------------------------------------------
+// Westfälische Hochschule - FB Maschinenbau
+// Labor für Mikrolektronik und Robotik
+// Modul Embedded Robotics -Prof. O. Just
+//--------------------------------------------------------
+// youBot_teleop_joy.cpp
+// Version vom 18.05.2021
+//--------------------------------------------------------
+// Unser erster ROS-Knoten in C++
+// Usage: 
+// Ordner "youBot_teleop_joy" in den catkin_ws/src verschieben
+// kompilieren mit $ catkin_make
+// starten mit rosrun emr youBot_teleop_joy
+//---------------------------------------------------------
+//#include </opt/ros/noetic/include/ros/ros.h>
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+
+
+class TeleopBot
+{
+public:
+  TeleopBot();
+
+private:
+  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+
+  ros::NodeHandle nh_;
+
+  int linear_, angular_;
+  double l_scale_, a_scale_;
+  ros::Publisher vel_pub_;
+  ros::Subscriber joy_sub_;
+
+};
+
+
+TeleopBot::TeleopBot():
+  linear_(1),
+  angular_(2)
+{
+  nh_.param("axis_linear", linear_, linear_);
+  nh_.param("axis_angular", angular_, angular_);
+  nh_.param("scale_angular", a_scale_, a_scale_);
+  nh_.param("scale_linear", l_scale_, l_scale_);
+
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopBot::joyCallback, this);
+}
+
+void TeleopBot::joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
+  geometry_msgs::Twist twist;
+  twist.angular.z = a_scale_*joy->axes[angular_];
+  twist.linear.x = l_scale_*joy->axes[linear_];
+  vel_pub_.publish(twist);
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "teleop_bot");
+  TeleopBot teleop_bot;
+
+  ros::spin();
+}
